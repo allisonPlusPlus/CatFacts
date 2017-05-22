be5d082622298b52850173edc3d75ff168d51f8e
theallisonbrett <theallisonbrett@gmail.com>
2017-05-21 21:01:39 -0700

    Solve access-control-allow-origin error by adding proxy

--- catfact.js
+++ catfact.js
@@ -1,18 +1,23 @@
 $(document).ready(function() {
 
-var getFact = function() {
-  $.ajax({
-      url: "https://catfacts-api.appspot.com/api/facts",
-      success: function( response ) {
-        var factObject = JSON.parse(response);
-        var factString = factObject.facts.join();
-        $("#fact").hide().fadeIn(1000);
-        $("#fact").html(factString);
-        $("#tweet").attr("href", "https://twitter.com/home/?status=" +factString);
-      }
-  });
-};
-getFact();
 
-  $("#new").click(getFact);
-      });
+
+    var getFact = function() {
+        var myUrl = 'https://catfacts-api.appspot.com/api/facts';
+        var proxy = 'https://cors-anywhere.herokuapp.com/';
+
+        $.ajax({
+            url: proxy + myUrl,
+            success: function(response) {
+                var factObject = JSON.parse(response);
+                var factString = factObject.facts.join();
+                $("#fact").hide().fadeIn(1000);
+                $("#fact").html(factString);
+                $("#tweet").attr("href", "https://twitter.com/home/?status=" + factString);
+            }
+        });
+    };
+    getFact();
+
+    $("#new").click(getFact);
+});
