5b3174fa611c542dbabe0c3a9ef79e6a8f73ecb8
theallisonbrett <theallisonbrett@gmail.com>
2017-05-21 18:48:11 -0700

    Update so that cat fact displays upon loading page

--- catfact.js
+++ catfact.js
@@ -1,10 +1,19 @@
 $(document).ready(function() {
+  $.ajax({
+      url: "https://catfacts-api.appspot.com/api/facts",
+      success: function( response ) {
+        var factObject = JSON.parse(response);
+        var factString = factObject.facts.join();
+        $("#fact").html(factString);
+      }
+  });
+
   $("#new").click(function(){
       $.ajax({url: "https://catfacts-api.appspot.com/api/facts", success: function(result){
           var factObject = JSON.parse(result);
           var factString = factObject.facts.join();
           $("#fact").html(factString);
-          $("#tweet").attr("href", "https://twitter.com/home/?status=" + factString);
+          $("#tweet").attr("href", "https://twitter.com/home/?status=" +factString);
       }});
 
   });
