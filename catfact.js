f63040a6df54032a4835e135f7859e7a809ed939
theallisonbrett <theallisonbrett@gmail.com>
2017-05-21 17:50:22 -0700

    Turn JSON into an object and then isolate the fact to display on the string

--- catfact.js
+++ catfact.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
   $("#new").click(function(){
       $.ajax({url: "https://catfacts-api.appspot.com/api/facts", success: function(result){
-          $("#fact").html(result);
+          var factObject = JSON.parse(result);
+          var factString = factObject.facts.join();
+          $("#fact").html(factString);
       }});
   });
 });
