b6eaed02a15952532ad5aa1b0e939ed7de0e05a6
theallisonbrett <theallisonbrett@gmail.com>
2017-05-21 17:39:10 -0700

    Change HTTP to HTTPS

--- catfact.js
+++ catfact.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $("#new").click(function(){
-      $.ajax({url: "http://catfacts-api.appspot.com/api/facts", success: function(result){
+      $.ajax({url: "https://catfacts-api.appspot.com/api/facts", success: function(result){
           $("#fact").html(result);
       }});
   });
