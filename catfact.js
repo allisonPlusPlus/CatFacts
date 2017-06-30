f9febfd8dea88747b4e8cd8206cff55692a4d204
theallisonbrett <theallisonbrett@gmail.com>
2017-06-29 20:38:01 -0700

    Update defunct catfact api with new working catfact.info api

--- catfact.js
+++ catfact.js
@@ -1,23 +1,46 @@
+'use strict';
+
 $(document).ready(function() {
+  //
+  // $.ajax({
+  //     url: proxy + myUrl,
+  //     success: function(response) {
+  //
+  //         debugger;
+  //         var factString = response.facts;
+  //         $("#fact").hide().fadeIn(1000);
+  //         $("#fact").html(factString);
+  //         $("#tweet").attr("href", "https://twitter.com/home/?status=" + factString);
+  //
+  //     },
+  // });
 
+  let facts;
+  let currFact = 0;
+  let myUrl2 = 'https://catfacts-api.appspot.com/api/facts';
+  let myUrl = 'http://www.catfact.info/api/v1/facts.json?page=:page&per_page=:per_page';
+  let proxy = 'https://cors-anywhere.herokuapp.com/';
 
+  function getFact(currFact, facts) {
+    let factString = facts[currFact].details;
+    $("#fact").hide().fadeIn(1000);
+    $("#fact").html(factString);
+    $("#tweet").attr("href", "https://twitter.com/home/?status=" + factString);
+    if (currFact >= 7){
+      return 0
+    }
+    return currFact + 1;
+  };
 
-    var getFact = function() {
-        var myUrl = 'https://catfacts-api.appspot.com/api/facts';
-        var proxy = 'https://cors-anywhere.herokuapp.com/';
+  $.get(proxy + myUrl)
+  .done(function(response){
+    facts = response.facts;
+    currFact = getFact(currFact, facts);
+  })
+  .done(function(){
+    $("#new").click(function(){
+      currFact = getFact(currFact, facts);
+    });
+  })
 
-        $.ajax({
-            url: proxy + myUrl,
-            success: function(response) {
-                var factObject = JSON.parse(response);
-                var factString = factObject.facts.join();
-                $("#fact").hide().fadeIn(1000);
-                $("#fact").html(factString);
-                $("#tweet").attr("href", "https://twitter.com/home/?status=" + factString);
-            }
-        });
-    };
-    getFact();
-
-    $("#new").click(getFact);
 });
